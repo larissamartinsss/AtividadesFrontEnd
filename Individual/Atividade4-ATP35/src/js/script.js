69f6032a85e6e63fbebd99ae5294c168707f28bb
Larissa Martins <larissamartins.job@gmail.com>
2021-11-03 08:49:55 -0400

    adicionando js

--- Individual/Atividade4-ATP35/src/js/script.js
+++ Individual/Atividade4-ATP35/src/js/script.js
@@ -6,3 +6,28 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+function index(){
+    let frame = document.getElementById("frame");
+    frame.src="index.html"
+}
+
+function aboutme(){
+    let frame = document.getElementById("frame");
+    frame.src="about-me.html"
+}
+
+function contactme(){
+    let frame = document.getElementById("frame");
+    frame.src="contact-me.html"
+}
+
+function projects(){
+    let frame = document.getElementById("frame");
+    frame.src="projects.html"
+}
+
+function skills(){
+    let frame = document.getElementById("frame");
+    frame.src="skills.html"
+}
